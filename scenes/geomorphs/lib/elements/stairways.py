c224780d643d2beac05c5df5965550e50a525364
autowitch <autowitch@autowit.ch>
2013-08-18 21:24:43 -0600

    S000: Playing with a vertical entrance
    Change-Id: I69c2d406c0359516cc1fbdff180daa6924f7e9ec

--- scenes/geomorphs/lib/elements/stairways.py
+++ scenes/geomorphs/lib/elements/stairways.py
@@ -1,9 +1,29 @@
-from pypov.pov import Cylinder, Box, Intersection, Difference, Union
+from pypov.pov import Cylinder, Box, Intersection, Difference, Union, Object
+from lib.util import float_range
+from lib.base import wall_texture_1
 
-def circular_stairs(height=10, radius=5, core_radius=1.5):
+def circular_stairs(height=10, radius=5, core_radius=1.5, step_height=1,
+        step_angle=10, start_angle=0, step_texture=wall_texture_1,
+        core_texture=wall_texture_1, wall_texture=wall_texture_1,
+        hollow_core=False):
+
+    steps = Union()
+    angle = start_angle
+    for x in float_range(0, height, step_height):
+        steps.append(
+            Box((0, x, 0), (radius, x + step_height, radius),
+                rotate=(0, angle, 0)
+            )
+        )
+        angle += step_angle
+
 
     stairway = Union(
-            Cylinder((0, 0, 0), (0, height, 0), core_radius)
+            Object(Cylinder((0, 0, 0), (0, height, 0), core_radius), core_texture),
+            Intersection(
+                Object(steps, step_texture),
+                Object(Cylinder((0, 0, 0), (0, height, 0), radius), wall_texture)
+            )
     )
 
     return stairway
--- scenes/geomorphs/lib/geomorphs/corner_entrance_5x5_002.py
+++ scenes/geomorphs/lib/geomorphs/corner_entrance_5x5_002.py
@@ -1,6 +1,6 @@
 
-from pypov.pov import Texture, Pigment
-from pypov.pov import Finish, Box, Cone, Object
+from pypov.pov import Texture, Pigment, Isosurface, Function, Intersection
+from pypov.pov import Finish, Box, Cone, Object, Cylinder
 from pypov.pov import Union, Difference
 from pypov.colors import Colors
 
@@ -23,21 +23,88 @@ def corner_entrance_5x5_002(rotate=(0, 0, 0), translate=(0, 0, 0), detail_level=
         cross_hatch_texture=cross_hatch_2):
 
     """docstring for gm02"""
+
+    step_texture = Texture(Pigment(color=Colors.Gray50))
     geomorph = Union(
         Difference(
             Object(five_by_five_corner, cross_hatch_texture),
             Union(
-                Box(( -5, 10,  -5), (  5, 21, -26)),
-                Box((  5.0001, 10,  -5), (-26, 21,   5)),
-                Box((-20, 10.000001, -20), ( 20, 21,  20)),
+                #Box(( -5, 10,  -4.9999), (  5, 21, -26)), # hall 1
+                #Box((  5.0001, 10,  -5), (-26, 21,   5)), # hall 2
+                #Box((-20, 10.000001, -20), ( 20, 21,  20)), # big room
+
+                # Halls
+                Box(( -5, 10,  -17), (  5, 21, -26)),
+                Box((  -12, 10,  -5), (-26, 21,   5)),
+
+                # Stair Base
+                Cylinder((15, 10, 0), (15, 21, 0), 5),
+
+                # Rooms
+                Box((8, 10.0002, -15), (-10, 21, 10)),
+                Box((8, 10.0002, 12), (-10, 21, 23)),
+                Box((23, 10.0002, 7), (10, 21, 23)),
+
+                # Doors
+                Box((15, 10.00003, 5), (-9, 16, 1.5)),
+                Cylinder((15, 15, 3.25), (0, 15, 3.25), 2),
+                Box((-2, 10.00002, -18), (2, 16, 14)),
+                Cylinder((0, 16, -18), (0, 16, 14), 2),
+                Box((-13, 10.00001, -2), (-9, 16, 2)),
+                Cylinder((-13, 16, 0), (-9, 16, 0), 2),
+                Box((11, 10.000003, 21), (7, 15, 18)),
+                Cylinder((11, 15, 19.5), (7, 15, 19.5), 1.5),
+
                 wall_texture_1
-            )
+            ),
         ),
         Object(
-            circular_stairs(),
-            Texture(wall_texture_1),
-            translate=(0,10,0),
+            circular_stairs(height=50, step_height=0.75, step_angle=15,
+                step_texture=step_texture),
+            translate=(15,10,0),
         ),
+        Difference(
+            Cylinder((15, 20, 0), (15, 60, 0), 7),
+            Object(Cylinder((15, 19, 0), (15, 61, 0), 5), wall_texture_1),
+            Box((15, 19, -8), (8, 61, 8)),
+            cross_hatch_texture
+        ),
+        Object(
+            Difference(
+    #            Isosurface(Function("y + f_noised3d(x*2, 0, z*2)"),
+                Isosurface("function { abs(y + f_noise3d(x / 20, 0, z / 20) * 15) - 1 } contained_by { box { <-10, -20, -15>, <25, 20, 15> } }",
+    #                contained_by=Box((-20, -20, -20), (20, 20, 20)),
+                    translate=(0, 61, 0),
+                ),
+                Cylinder((15, 20, 0), (15, 70, 0), 5.01),
+            ),
+            Texture(Pigment(color=Colors.DarkOliveGreen)),
+        ),
+        Union(
+            Difference(
+                Box((24, 58, -8), (-2, 69.999, 8)),
+                Cylinder((15, 57, 0), (15, 62, 0), 5.011),
+                Box((23, 60.00001, -7), (10, 70, 7)),
+                Box((9, 60.5, -7), (-1, 70, 7)),
+                Box((-3, 60.500001, -2), (0, 65, 2)),
+                Intersection(
+                    Cylinder((-3, 65, -2), (0, 65, -2), 4),
+                    Cylinder((-3, 65, 2), (0, 65, 2), 4),
+                ),
+                Box((8, 60.500001, 2), (11, 65, 6)),
+                Intersection(
+                    Cylinder((8, 65, 2), (11, 65, 2), 4),
+                    Cylinder((8, 65, 6), (11, 65, 6), 4),
+                ),
+                Box((2, 64, 9), (5, 67.5, -9)),
+                Cylinder((3.5, 67.5, 9), (3.5, 67.5, -9), 1.5)
+            ),
+            Box((-2, 58, -2), (-3, 60.5, 2)),
+            Box((-3, 58, -2), (-4, 60, 2)),
+            Box((-4, 58, -2), (-5, 59.5, 2)),
+            Box((-5, 58, -2), (-6, 59, 2)),
+        ),
+        wall_texture_1,
         translate=translate,
         rotate=rotate
     )
