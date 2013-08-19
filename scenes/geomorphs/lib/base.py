9c99fb67c73019901f98e29be4e571a53869c0b9
autowitch <autowitch@autowit.ch>
2013-08-19 16:16:32 -0600

    S00: Breakin' stuff
    Change-Id: I1724fd46802eeefef90fd2f868c8069e8acf5f73

--- scenes/geomorphs/lib/base.py
+++ scenes/geomorphs/lib/base.py
@@ -1,108 +1,121 @@
-from pypov.pov import Box, Difference, Texture, Pigment, Union
+from pypov.pov import Box, Difference, Texture, Pigment, Union, Object
 from pypov.pov import Normal, Checker, Brick, Wrinkles, Finish
 from pypov.pov import Interior, Material
 from pypov.common import grey
 from pypov.colors import Colors
 
-# Basic checkerboard pattern on a 5x5 grid. tiles well.
-cross_hatch = Texture(
-    Pigment(Checker(Colors.LightWood, Colors.MediumWood)),
-    scale=5
-)
-
-# Completely transparent.
-cross_hatch_2 = Texture(
-    Pigment(color=Colors.Clear)
-)
-
-# Glass
-cross_hatch_3 = Material(
-    Texture(
-        Pigment(
-            filter=0.7,
-            transmit=0.7,
-            color=(0.8, 1, 0.95, 0.9), # Quirky little bug here. The color argument
-                                       # in POVRay must be listed FIRST, however, the
-                                       # way the argument list is built makes it necessary
-                                       # to put it last.
-        ),
-        Finish(
-            ambient=0.1,
-            diffuse=0.1,
-            reflection=0.1,
-            specular=0.8,
-            roughness=0.0003,
-            phong=1,
-            phong_size=400,
+def five_by_five_full(highlight_exits=False):
+    exits = Union(
+        Box(( -2.499, 10.01,  24.75), (  2.499,  21,  26)),
+        Box(( -2.499, 10.01, -24.75), (  2.499,  21, -26)),
+        Box((-24.75, 10.01,  -2.499), (-26,     21,   2.499)),
+        Box(( 24.75, 10.01,  -2.499), ( 26,     21,   2.499)),
+    )
+    if highlight_exits:
+        exits = Union(
+            Box(( -2.499, 10.01,  24.75), (  2.499,  21,  26), Pigment(color=Colors.Red)),
+            Box(( -2.499, 10.01, -24.75), (  2.499,  21, -26), Pigment(color=Colors.Red)),
+            Box((-24.75, 10.01,  -2.499), (-26,     21,   2.499), Pigment(color=Colors.Red)),
+            Box(( 24.75, 10.01,  -2.499), ( 26,     21,   2.499), Pigment(color=Colors.Red)),
         )
-    ),
-    Interior(
-        ior=1.5
+    obj = Difference( # 4 exits
+        Box((-25,     0,    -25),    ( 25,     20,  25)),
+        exits
     )
-)
+    return obj
 
-# Wall Texture 1 - sandstone brick
-
-wall_texture_1 = Texture(
-    Pigment(
-        Brick(
-            Colors.Gray15, Colors.DarkTan,
-            brick_size = (2.5, 1.5, 2.5),
-            mortar = 0.1
-        ),
-        #Normal(
-            #Wrinkles(0.75, scale=0.1)
-        #),
-        #Finish(diffuse=0.9, phong=0.2)
-        translate=(0.15, 0.15, 0.15)
+def five_by_five_corner(highlight_exits=False):
+    exits = Union(
+        Box(( -4.99, 10.01, -24.75), (  4.99, 21, -26)),
+        Box((-24.75, 10.01,  -4.99), (-26,    21,   4.99)),
     )
-)
+    if highlight_exits:
+        exits = Union(
+            Box(( -4.99, 10.01, -24.75), (  4.99, 21, -26), Pigment(color=Colors.Red)),
+            Box((-24.75, 10.01,  -4.99), (-26,    21,   4.99), Pigment(color=Colors.Red)),
+        )
+    obj = Difference( # two exits 90 degrees
+        Box((-25,    0,     -25),    ( 25,    20,  25)),
+        exits,
+    )
+    return obj
 
-five_by_five = Difference(
-    Box((-25,     0,    -25),    ( 25,     20,  25)),
-    Box(( -4.99, 10.01,  24.75), (  4.99,  21,  26)),
-    Box(( -4.99, 10.01, -24.75), (  4.99,  21, -26)),
-    Box((-24.75, 10.01,  -4.99), (-26,     21,   4.99)),
-    Box(( 24.75, 10.01,  -4.99), ( 26,     21,   4.99)),
-)
+def five_by_five_pipe(highlight_exits=False):
+    exits = Union(
+        Box(( -4.99, 10.01, -24.75), (  4.99, 21, -26)),
+        Box(( -4.99, 10.01,  24.75), (  4.99, 21, 26)),
+    )
+    if highlight_exits:
+        exits = Union(
+            Box(( -4.99, 10.01, -24.75), (  4.99, 21, -26), Pigment(color=Colors.Red)),
+            Box(( -4.99, 10.01,  24.75), (  4.99, 21, 26), Pigment(color=Colors.Red)),
+        )
+    obj= Difference( # two exits 180 degrees
+        Box((-25,    0,     -25),    ( 25,    20,  25)),
+        exits
+    )
+    return obj
 
-five_by_five_corner = Difference(
-    Box((-25,    0,     -25),    ( 25,    20,  25)),
-    Box(( -4.99, 10.01, -24.75), (  4.99, 21, -26)),
-    Box((-24.75, 10.01,  -4.99), (-26,    21,   4.99)),
-)
+def five_by_five_edge(highlight_exits=False): # three exits
+    exits = Union(
+        Box(( -4.99, 10.01, -24.75), (  4.99, 21, -26)),
+        Box(( -4.99, 10.01,  24.75), (  4.99, 21, 26)),
+        Box((-24.75, 10.01,  -4.99), (-26,    21,   4.99)),
+    )
+    if highlight_exits:
+        exits = Union(
+            Box(( -4.99, 10.01, -24.75), (  4.99, 21, -26), Pigment(color=Colors.Red)),
+            Box(( -4.99, 10.01,  24.75), (  4.99, 21, 26), Pigment(color=Colors.Red)),
+            Box((-24.75, 10.01,  -4.99), (-26,    21,   4.99), Pigment(color=Colors.Red)),
+        )
+    obj = Difference(
+        Box((-25,    0,     -25),    ( 25,    20,  25)),
+        exits,
+    )
+    return obj
 
+def five_by_five_dead_end(highlight_exits=False): # one exit
+    exits = Object(
+        Box(( -4.99, 10.01, -24.75), (  4.99, 21, -26))
+    )
+    if highlight_exits:
+        exits = Object(
+            Box(( -4.99, 10.01, -24.75), (  4.99, 21, -26), Pigment(color=Colors.Red)),
+        )
+    obj = Difference(
+        Box((-25,    0,     -25),    ( 25,    20,  25)),
+        exits,
+    )
+    return obj
 
-ten_by_ten = Difference(
+def five_by_ten(highlight_exits=False):
+    exits = Union(
+        Box((-49.75, 10.01,  -2.499), (-51,     21,   2.499)),
+        Box(( 49.75, 10.01,  -2.499), ( 51,     21,   2.499)),
+    )
+    if highlight_exits:
+        exits = Union(
+            Box((-49.75, 10.01,  -2.499), (-51,     21,   2.499), Pigment(color=Colors.Red)),
+            Box(( 49.75, 10.01,  -2.499), ( 51,     21,   2.499), Pigment(color=Colors.Red)),
+        )
+    obj = Difference(
+        Box((-50, -0, -25), (50, 20, 25)),
+        exits
+    )
+    return obj
+
+def ten_by_ten(highlight_exits=False):
+    obj = Difference(
         Box((-50, -0, -50), (50, 20, 50)),
+    )
+    return obj
 
-)
-    #geomorph = Union(
-        #Texture(
-            #Pigment(color=(0.025, 0.025, 0.025)),
-            #Finish(reflection=0.05)
-        #),
-        #translate=translate,
-        #rotate=rotate
-    #)
-    #return geomorph
+def ten_by_twenty(highlight_exits=False):
+    obj = Difference(
+    )
+    return obj
 
-class TenXTen(object):
-    """docstring for TenXTen"""
-    def __init__(self):
-        super(TenXTen, self).__init__()
-
-class TenXFive(object):
-    """docstring for TenXFive"""
-    def __init__(self):
-        super(TenXFive, self).__init__()
-
-class FiveByFive(object):
-
-    """docstring for FiveByFive"""
-    def __init__(self):
-        super(FiveByFive, self).__init__()
-
-
-
-
+def twenty_by_twenty(highlight_exits=False):
+    obj = Difference(
+    )
+    return obj
---
+++ scenes/geomorphs/obj_test.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python
+
+import os
+import sys
+# Assumes that a symlink to the pov modules is in the lib dir
+
+import argparse
+from pypov.pov import File, Settings, POV, parse_args, Camera, Union
+from lib.environment import general, ground
+from lib.textures import cross_hatch, cross_hatch_2, cross_hatch_3
+from lib.base import five_by_five_full
+from lib.base import five_by_five_corner
+from lib.base import five_by_five_pipe
+from lib.base import five_by_five_edge
+from lib.base import five_by_five_dead_end
+from lib.base import five_by_ten
+from lib.base import ten_by_ten
+from lib.base import ten_by_twenty
+from lib.base import twenty_by_twenty
+
+
+def main():
+    """docstring for main"""
+    parser = argparse.ArgumentParser(description='Render a single geomorph',
+            prefix_chars='+-')
+    parser.add_argument('-D', '--detail-level',
+                        type=int,
+                        default=10,
+                        dest='detail_level',
+                        help='Set the level of detail fo the scene')
+    parser.add_argument('-g', '--geomorph',
+                        type=str,
+                        default='gm01',
+                        dest='geomorph',
+                        help='Select the geomorph to renger')
+    parser.add_argument('--list',
+                        default=None,
+                        action='store_true',
+                        dest='list_geomorphs',
+                        help='List all geomorphs')
+    parser.add_argument('--info', default=None,
+                        action='store_true',
+                        dest='info',
+                        help='Display information about a specific geomorph')
+    parser.add_argument('--earth-texture', default=None,
+                        type=str,
+                        choices=['checker', 'glass', 'clear'],
+                        dest='earth_texture',
+                        help='Set the texture the geomorph is rendered inside')
+    parser.add_argument('--ground-offset',
+                        default=0,
+                        type=int,
+                        dest='ground_offset',
+                        help='Set the offset to the ground plane')
+
+    parser.add_argument('-R', '--rotate',
+                        nargs=3,
+                        type=float,
+                        default=None,
+                        dest='rotation',
+                        metavar=('X', 'Y', 'Z'),
+                        help='Set camera location')
+    args = parse_args(parser)
+
+    if args.list_geomorphs:
+        path = os.path.realpath(__file__)
+        path = "%s/lib/geomorphs" % path[:path.rfind('/')]
+        print "geomorphs found in %s:" % path
+        # How silly can we make this?
+        geomorphs = \
+                '\n'.join(
+                    sorted(
+                        map(lambda x : x[:x.rfind('.')],
+                            filter(
+                                lambda x : x.endswith('.py') and \
+                                           not x.startswith('_'),
+                                os.listdir(path)
+                            )
+                        )
+                    )
+                )
+        print geomorphs
+        sys.exit(0)
+
+
+    earth_texture = cross_hatch_2
+    if args.earth_texture:
+        textures = {'glass':cross_hatch_3,
+                'checker':cross_hatch,
+                'clear':cross_hatch_2,
+        }
+        earth_texture = textures[args.earth_texture]
+
+    rotation = (0, 0, 0)
+    if args.rotation:
+        rotation = args.rotation
+
+    pov_file = File("geomorph-test.pov")
+    pov_file.include("colors.inc")
+    pov_file.include("stones.inc")
+    pov_file.include("metals.inc")
+    pov_file.include("functions.inc")
+    general(pov_file)
+    ground(pov_file, offset=args.ground_offset)
+    camera = Camera(
+        location = (-150, 250, -60),
+        look_at = (0, 25, 0)
+    )
+    camera.write(pov_file)
+
+    tile = Union(
+        five_by_ten(highlight_exits=True),
+        earth_texture,
+    )
+    tile.write(pov_file)
+
+    settings = Settings()
+    renderer = POV()
+    renderer.render(pov_file, settings)
+
+
+if __name__ == '__main__':
+    main()
